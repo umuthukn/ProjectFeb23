0bfd34cfa2e6de11ad08c4aac75793d8d09522d6
umuthukn <umuthukn@gmail.com>
2023-02-20 16:56:42 +0530

    Kumaran code

--- src/test/java/org/test/Shopclues.java
+++ src/test/java/org/test/Shopclues.java
@@ -4,5 +4,8 @@ public class Shopclues {
 
 	public static void main(String[] args) {
 		System.out.println("test1");
+		System.out.println("test4");
+		System.out.println("test5");
+		System.out.println("test6");
 	}
 }
